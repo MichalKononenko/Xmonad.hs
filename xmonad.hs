9f68233920affece2604ab21d784d856ee384b1f
Michal Kononenko <michalkononenko@gmail.com>
2016-08-21 22:20:22 -0400

    Replaced horizontal tiling with Fibonnaci spiral

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Layout.ShowWName
+import XMonad.Layout.Spiral
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
@@ -15,13 +16,32 @@ myManageHook = composeAll
     , className =? "zotero" --> doShift "8:zotero"
     ]
 
+-- Define the tiling algorithm
+fullscreen = Full
+
+tiled = Tall nmaster delta ratio
+    where
+    -- Number of windows in the master pane
+    nmaster = 1
+
+    -- Proportion of screen occupied by master pane
+    ratio = 1/2
+
+    -- Percent of screen to increment
+    delta = 3/100
+
+fibonnaci_spiral = spiral(500/809)
+
+
+myLayout = fullscreen ||| tiled ||| fibonnaci_spiral
+
 main = do
     -- Spawn the status bar at the top of the screen using the xmobarrc in this directory
     xmproc <- spawnPipe "/usr/bin/xmobar /home/mkononen/.xmonad/xmobar.hs" 
     
     xmonad $ defaultConfig {
         manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig 
-      , layoutHook = avoidStruts $ showWName (layoutHook defaultConfig)
+      , layoutHook = avoidStruts $ showWName  myLayout
       , logHook = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc
             , ppTitle = xmobarColor "#657b83" "" . shorten 0
@@ -49,9 +69,12 @@ main = do
           -- Let shift + volume keys control the brightness level 
         , ((1, 0x1008FF11), spawn "xbacklight -dec 20")
         , ((1, 0x1008FF13), spawn "xbacklight -inc 20")
+        
+          -- Spawn the menu with appropriate parameters
         , ((mod1Mask, xK_p), spawn "dmenu_run -fn \
             \ \"xft:Roboto Regular:size=9:antialias=true\" \
-            \ -nb \"#000000\" -nf \"#657b83\" "
+            \ -nb \"#000000\" -nf \"#657b83\" \
+            \ -sb \"yellow\" -sf \"#000000\" "
           )
         ]
 
