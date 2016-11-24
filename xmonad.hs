c3298dd0cc2552c8754bed32b37e9c69828f95c8
MichalKononenko <michalkononenko@gmail.com>
2016-11-24 15:58:37 -0500

    Added a border color and switched to xfce4-term

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,22 @@
 import XMonad
 import XMonad.Config.Xfce
 
+-- Set up workspaces
+myWorkspaces = ["1:main", "2:web", "3:code", "4:testing", "5", "6", "7:email", "8:zotero"]
+myManageHook = composeAll
+    [
+        className =? "Thunderbird" --> doShift "7:email"
+    ,   className =? "Zotero" --> doShift "8:zotero"
+    ]
+
 main = xmonad xfceConfig
     {
-        terminal = "urxvt"
+        terminal = "xfce4-terminal"
+    ,   manageHook = myManageHook <+> manageHook xfceConfig
     ,   modMask  = mod4Mask
+    ,   workspaces = myWorkspaces
+    ,   borderWidth = 2
+    ,   normalBorderColor = "#d1d1d1"
+    ,   focusedBorderColor = "#75e468"
     }
+
