c7e4364988c5fa37390c4bcfa0e0ff3c93dd07a4
MichalKononenko <michalkononenko@gmail.com>
2016-11-26 13:03:10 -0500

    Brought back the functionality for XFCE

--- xmonad.hs
+++ xmonad.hs
@@ -1,22 +1,53 @@
 import XMonad
 import XMonad.Config.Xfce
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.ShowWName
+import XMonad.Layout.Spiral
+import System.IO
+
 
 -- Set up workspaces
-myWorkspaces = ["1:main", "2:web", "3:code", "4:testing", "5", "6", "7:email", "8:zotero"]
+myWorkspaces = ["1:main", "2:web", "3:code", "4:testing", "5:libreoffice", "6", "7:email", "8:zotero"]
+
 myManageHook = composeAll
     [
         className =? "Thunderbird" --> doShift "7:email"
     ,   className =? "Zotero" --> doShift "8:zotero"
     ]
 
-main = xmonad xfceConfig
-    {
-        terminal = "xfce4-terminal"
-    ,   manageHook = myManageHook <+> manageHook xfceConfig
-    ,   modMask  = mod4Mask
-    ,   workspaces = myWorkspaces
-    ,   borderWidth = 2
-    ,   normalBorderColor = "#d1d1d1"
-    ,   focusedBorderColor = "#75e468"
-    }
+tiled = Tall nmaster delta ratio
+    where
+    -- number of windows in the master pane
+    nmaster = 1
+    -- Proportion of screen occupied by master pane
+    ratio = 1/2
+    -- Percent of screen to increment
+    delta = 3/100
+
+fibonacci_spiral = spiral(500/809)
+
+myLayout = fibonacci_spiral ||| Full ||| tiled
+
+main = do
+    xmonad $ xfceConfig {
+            terminal = "xfce4-terminal"
+        ,   manageHook = myManageHook <+> manageHook xfceConfig
+        ,   layoutHook = showWName myLayout
+        ,   modMask  = mod4Mask
+        ,   workspaces = myWorkspaces
+        ,   borderWidth = 2
+        ,   normalBorderColor = "#d1d1d1"
+        ,   focusedBorderColor = "#75e468"
+    } `additionalKeys` 
+        [
+            -- Toggle switch for muting the microphone
+            ((0, 0x1008FFB2), spawn "amixer set Mic toggle")
+            
+            -- Brightness Controls
+        ,   ((1, 0x1008FF11), spawn "xbacklight -dec 20")
+        ,   ((1, 0x1008FF13), spawn "xbacklight -inc 20")
+            -- Use the old "slock" button to start music
+        ,   ((0, 0x1008FF41), spawn "mpc toggle")
+        ]
 
